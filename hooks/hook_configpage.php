60a6a5e6ff9f2e6c86894acd1d5e52990f5ef6fa
Tim van Dijen <tvdijen@gmail.com>
2022-01-08 20:21:03 +0100

    Fix hooks

---
+++ hooks/hook_configpage.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Module;
+use SimpleSAML\XHTML\Template;
+
+/**
+ * Hook to add the metaedit module to the config page.
+ *
+ * @param \SimpleSAML\XHTML\Template &$template The template that we should alter in this hook.
+ */
+function metaedit_hook_configpage(Template &$template): void
+{
+    $template->data['links'][] = [
+        'href' => Module::getModuleURL('metaedit/index.php'),
+        'text' => [Translate::noop('Metadata registry')],
+    ];
+
+    $template->getLocalization()->addModuleDomain('metaedit');
+}
